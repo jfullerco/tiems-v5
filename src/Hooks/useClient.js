edb620de7e455996e2f08184cf4eb53269248b05
jfullerco <jonathan@jfuller.co>
2021-02-14 20:23:22 -0500

    2 files modified

---
+++ src/Hooks/useClient.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {useQuery,
+        useMutation,
+        useQueryClient,
+        QueryClient,
+        QueryClientProvider} from 'react-query'
+
+const useClient = () => {
+  const queryClient = useQueryClient
+  const query = 
+}
