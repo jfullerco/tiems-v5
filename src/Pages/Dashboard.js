b223c7f484d75d5d336a5f1b58e4243eb9f8327d
jfullerco <jonathan@jfuller.co>
2021-02-23 08:52:25 -0500

    2 files modified

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useContext, useRef} from 'react'
+import React, {useState, useEffect, useContext} from 'react'
 import {Route, Link, Switch} from 'react-router-dom'
 import {StateContext, saveMe} from '../stateContext'
 import getClient from '../Services/clientService'
--- src/stateContext.js
+++ src/stateContext.js
@@ -1,4 +1,4 @@
-import React, {useState, createContext, useRef} from 'react'
+import React, {useState, createContext} from 'react'
 
 export const StateContext = createContext({})
 
